05375e1f1ec35cbb83ede83ab7acde1dc9ce8727
DESKTOP-GJTA02F\tuong <tuongtran@liontech.vn>
2024-04-24 11:41:02 +0700

    Add comma

--- backend/jobs/workers/fb/fb_empty_audience_create.py
+++ backend/jobs/workers/fb/fb_empty_audience_create.py
@@ -6,10 +6,17 @@ class FBEmptyAudienceCreate(worker.Worker):
         ('account_id', 'string', True, 'Ads Account Id'),
         ('app_id', 'string', True, 'FB App Id'),
         ('app_secret', 'string', True, 'FB App Secret'),
-        ('access_token', 'string', True, 'Access Token')
-        ('audience_name', 'string', True, 'Audience Name')
+        ('access_token', 'string', True, 'Access Token'),  # Added comma here
+        ('audience_name', 'string', True, 'Audience Name'),
         ('audience_description', 'string', True, 'Audience Description')
     ]
     
     def _execute(self) -> None:
-        fb_utils.create_empty_custom_audience_create(self._params['account_id'], self._params['app_id'], self._params['app_secret'], self._params['access_token'], self._params['audience_name'], self._params['audience_description'])
+        fb_utils.create_empty_custom_audience_create(
+            self._params['account_id'], 
+            self._params['app_id'], 
+            self._params['app_secret'], 
+            self._params['access_token'], 
+            self._params['audience_name'], 
+            self._params['audience_description']
+        )
