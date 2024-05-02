93feec3abbf5364f15476ddba7ad79d100cf3aa9
DESKTOP-GJTA02F\tuong <tuongtran@liontech.vn>
2024-05-02 08:49:46 +0700

    Update finder

--- backend/jobs/workers/finder.py
+++ backend/jobs/workers/finder.py
@@ -39,6 +39,7 @@ from jobs.workers.vertexai import vertexai_waiter
 from jobs.workers.vertexai import vertexai_worker
 from jobs.workers.fb import fb_empty_audience_create
 from jobs.workers.fb import fb_audiences_updater
+from jobs.workers.bigquery import bq_loop
 
 ConcreteWorker = TypeVar('ConcreteWorker', bound=worker.Worker)
 
@@ -46,6 +47,7 @@ WORKERS_MAPPING = {
     # 'AutoMLImporter',
     # 'AutoMLPredictor',
     # 'AutoMLTrainer',
+    'TestLoop': bq_loop.BQScriptExecutor,
     'FBAudiencesUpdater': fb_audiences_updater.FbAudiencesUpdater,
     'FBEmptyAudienceCreator': fb_empty_audience_create.FBEmptyAudienceCreate,
     'BQMLTrainer':
