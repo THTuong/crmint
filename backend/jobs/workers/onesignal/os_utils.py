0f3c8814a06026799fbc10260fde774b7bdeddb7
DESKTOP-GJTA02F\tuong <tuongtran@liontech.vn>
2024-07-16 11:43:38 +0700

    Update 16/07/2024

--- backend/jobs/workers/onesignal/os_utils.py
+++ backend/jobs/workers/onesignal/os_utils.py
@@ -1,33 +1,47 @@
 import requests
 import json
 
+def get_subscription_ids(api_url):
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        response_json = response.json()
+        subscription_ids = [item['attributes']['user_pseudo_id'] for item in response_json['data']]
+        return subscription_ids
+    else:
+        return {"error": "failed to get subscription ids", "status_code": response.status_code, "response": response.json()}
+
+def chunk_list(lst, chunk_size):
+    for i in range(0, len(lst), chunk_size):
+        yield lst[i:i + chunk_size]
+
 def send_notification(app_id, subscription_ids, contents_str, headings_str, chrome_web_image, url):
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Basic MWZhMjQ3NDQtY2FhMy00MzUzLWIyMzctMDVjYjkyMWJmNzRl"
+        "Authorization": "Basic YWNjMjA4ZWMtZjEzNS00ODk3LTkzYmItMWRlYzVhNDI1ZWZm"
     }
 
     # Convert input strings to dictionaries
-    # data = json.loads(data_str)
     contents = json.loads(contents_str)
     headings = json.loads(headings_str)
+    subscription_arr = get_subscription_ids(subscription_ids)
+    
+    for chunk in chunk_list(subscription_arr, 1000):
+        payload = {
+            "app_id": app_id,
+            "include_subscription_ids": chunk,
+            "contents": contents,
+            "headings": headings,
+            "chrome_web_image": chrome_web_image,
+            "url": url
+        }
 
-    payload = {
-        "app_id": app_id,
-        "include_subscription_ids": subscription_ids.split(","),
-        "contents": contents,
-        "headings": headings,
-        "chrome_web_image": chrome_web_image,
-        "url": url
-    }
-
-    response = requests.post('https://onesignal.com/api/v1/notifications', headers=headers, json=payload)
-    if response.status_code == 200:
-        response_json = response.json()
-        push_id = response_json['id']
-        status_url = f"https://api.onesignal.com/notifications/{push_id}?app_id={app_id}"
-        
-        status_response = requests.get(status_url, headers=headers)
-        return status_response.json()
-    else:
-        return {"error": "failed to push notification", "status_code": response.status_code, "response": response.json()}
+        response = requests.post('https://onesignal.com/api/v1/notifications', headers=headers, json=payload)
+        if response.status_code == 200:
+            response_json = response.json()
+            push_id = response_json['id']
+            status_url = f"https://api.onesignal.com/notifications/{push_id}?app_id={app_id}"
+            
+            status_response = requests.get(status_url, headers=headers)
+            return status_response.json()
+        else:
+            return {"error": "failed to push notification", "status_code": response.status_code, "response": response.json()}
--- backend/jobs/workers/onesignal/push_web_notification.py
+++ backend/jobs/workers/onesignal/push_web_notification.py
@@ -5,7 +5,7 @@ import requests
 class PushWebNotification(bq_worker.BQWorker):
     """Worker to update FB audiences using values from a BigQuery table."""
     PARAMS = [
-        ('app_id', 'string', True, '634a14e3-11d3-48a5-bea6-42dcbcaf69b1', 'App ID'),
+        ('app_id', 'string', True, 'fb5bd557-43ac-4655-a295-e009908a8b06', 'App ID'),
         ('include_subscription_ids', 'string', True, '', 'Subscription IDs'),
         ('contents', 'text', True, '', 'Content of notification'),
         ('chrome_web_image', 'string', True, '', 'Image of notification'),
